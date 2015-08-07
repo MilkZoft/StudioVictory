a84ae1590cc17c265bd6d9ab1323a35504fba483
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-08-07 16:40:35 -0700

    Adding grunt tasks & error view

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ var config = require('./lib/config');
 // Body Parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // Logger
 var logger = require('morgan');
