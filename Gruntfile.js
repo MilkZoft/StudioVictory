a977b14693633434739623bd2a6745de9bcffc20
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-08-07 16:42:03 -0700

    Updating Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,4 +71,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['analyze']);
   grunt.registerTask('analyze', 'Validates code style', ['jshint', 'jscs']);
+  grunt.registerTask('status', 'Shows status of node processes', ['shell:serverStatus']);
+  grunt.registerTask('stop', 'Stop node processes', ['shell:serverStop']);
+  grunt.registerTask('start', 'Start node processes', ['shell:serverStart']);
+  grunt.registerTask('restart', 'Restart node processes', ['stop', 'start']);
+  grunt.registerTask('logs', 'Tail logs for all pm2 processes', ['shell:serverLogs']);
 };
